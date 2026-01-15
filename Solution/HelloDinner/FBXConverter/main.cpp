143d1ffdc570002d75771306f8f924fbc3368a15
yunseo <yunseo000129@gmail.com>
2026-01-15 16:09:31 +0900

    thirdparty 생성 및 라이브러리 인클루드 설정

---
+++ Solution/HelloDinner/FBXConverter/main.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+int main()
+{
+	int a = 1;
+	cout << a;
+}
---
+++ Solution/HelloDinner/FBXConverter/stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include "assimp\scene.h"
+#include "assimp\Importer.hpp"
+#include "assimp\postprocess.h"
+
+using namespace std;
