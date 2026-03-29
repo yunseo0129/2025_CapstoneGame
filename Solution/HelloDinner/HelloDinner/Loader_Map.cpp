a9971102625e493995a3598187b669d3ef730e40
yunseo <yunseo000129@gmail.com>
2026-03-29 12:00:32 +0900

    .

--- Solution/HelloDinner/HelloDinner/Loader_Map.cpp
+++ Solution/HelloDinner/HelloDinner/Loader_Map.cpp
@@ -190,9 +190,11 @@ HRESULT CLoader_Map::Load_MapData(const string& strJsonPath, _uint iLevelIndex)
 
 			// textureNames 배열을 이용하여 각 MatIdx에 텍스처 설정
 			char szDebug[512];
+#ifndef _DEBUG
 			sprintf_s(szDebug, "[LoadMap] Model has %u materials, textureNames has %zu entries\n",
 				pModel->Get_NumMaterials(), materialNames.size());
 			OutputDebugStringA(szDebug);
+#endif
 
 			for (_uint matIdx = 0; matIdx < (_uint)materialNames.size(); matIdx++)
 			{
@@ -208,15 +210,19 @@ HRESULT CLoader_Map::Load_MapData(const string& strJsonPath, _uint iLevelIndex)
 						m_pGameInstance->Clone_Prototype(
 							Engine::PROTOTYPE::PROTO_COMPONENT, iLevelIndex, strAlbedoTag, nullptr));
 
+#ifndef _DEBUG
 					sprintf_s(szDebug, "[LoadMap] matIdx=%u, matName=%s, albedoTag=%ls, pAlbedo=0x%p\n",
 						matIdx, matName.c_str(), strAlbedoTag.c_str(), pAlbedo);
 					OutputDebugStringA(szDebug);
+#endif // !_DEBUG
 
 					if (pAlbedo != nullptr)
 					{
 						HRESULT hr = pModel->Set_MaterialTexture(matIdx, (TextureType)TextureType_DIFFUSE, pAlbedo);
+#ifndef _DEBUG
 						sprintf_s(szDebug, "[LoadMap] Set_MaterialTexture(Albedo) result: 0x%08X\n", hr);
 						OutputDebugStringA(szDebug);
+#endif
 					}
 				}
 
@@ -230,15 +236,19 @@ HRESULT CLoader_Map::Load_MapData(const string& strJsonPath, _uint iLevelIndex)
 						m_pGameInstance->Clone_Prototype(
 							Engine::PROTOTYPE::PROTO_COMPONENT, iLevelIndex, strNormalTag, nullptr));
 
+#ifndef _DEBUG
 					sprintf_s(szDebug, "[LoadMap] matIdx=%u, matName=%s, normalTag=%ls, pNormal=0x%p\n",
 						matIdx, matName.c_str(), strNormalTag.c_str(), pNormal);
 					OutputDebugStringA(szDebug);
+#endif
 
 					if (pNormal != nullptr)
 					{
 						HRESULT hr = pModel->Set_MaterialTexture(matIdx, (TextureType)TextureType_NORMALS, pNormal);
+#ifndef _DEBUG
 						sprintf_s(szDebug, "[LoadMap] Set_MaterialTexture(Normal) result: 0x%08X\n", hr);
 						OutputDebugStringA(szDebug);
+#endif
 					}
 				}
 			}
