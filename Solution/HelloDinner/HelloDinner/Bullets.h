6e00b03dc3d24d8b6a8acb150c08f29a34c8731c
yunseo <yunseo000129@gmail.com>
2026-06-20 20:40:31 +0900

    총알객체 생성

---
+++ Solution/HelloDinner/HelloDinner/Bullets.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "ContainerObj.h"
+#include "Model.h"
+
+class CBullets final : public CGameObject
+{
+public:
+    struct BULLET_DESC : public CGameObject::GAMEOBJECT_DESC
+    {
+    };
+    struct BULLET
+    {
+        _vector vLook;
+        _vector vPos;
+        _vector vTarget;
+    };
+private:
+    CBullets(EngineContext* pContext);
+    CBullets(const CBullets& Prototype);
+    virtual ~CBullets() = default;
+
+public:
+    virtual HRESULT		Initialize_Prototype() override;
+    virtual HRESULT		Initialize(void* pArg) override;
+    virtual void		Priority_Update(_float fTimeDelta) override;
+    virtual void		Update(_float fTimeDelta) override;
+    virtual void		Late_Update(_float fTimeDelta) override;
+    virtual void		Render(ID3D12GraphicsCommandList* _commandList) override;
+
+private:
+    virtual HRESULT				Ready_Components();
+
+private:
+    vector<BULLET> vBullets;
+
+public:
+    static CBullets* Create(EngineContext* pContext);
+    virtual CGameObject* Clone(void* pArg);
+    virtual void Free() override;
+
+
+};
